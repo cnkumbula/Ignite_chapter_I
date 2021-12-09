7a3bea2f24885badb44729a8e1d4b874aec1f0f0
claudio nkumbula@gmail.com <klaudio.kumbula@gmail.com>
2021-12-09 10:15:52 +0200

    adding patch and delete routes

--- src/index.js
+++ src/index.js
@@ -101,17 +101,49 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   //return response.status(201).send();
   return response.status(201).json(user.todos);
 
-
-
-
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(400).json({error: 'ToDos not found!!!'})
+  }
+
+  todo.done = true
+
+  return response.json(todo)
+
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(400).json({error: 'ToDos not found!!!'})
+  }
+
+  user.todos.splice(todo, 1);
+
+  return response.json(user.todos);
+
+
+
+
+  
+
+
+
+
+
 });
 
 module.exports = app;
